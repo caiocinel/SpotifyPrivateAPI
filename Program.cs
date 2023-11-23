6e933dc998b09710c1b74a3242cc4917b175ef3a
Caillou <caioalencarcinel@gmail.com>
2023-11-22 23:32:09 -0300

    Visual Studio crashes :/

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using SpotifyAPI.Web;
+
+class Program
+{
+    static async Task Main()
+    {
+        //var spotify = new SpotifyClient("BQDtlBOKtytxJy_0CRhF4ERNVohe-1qv_TF-_8Q5q5mSBpmYycwdIFKip2pZtI1F3NAzvxUxTDKNdywbJInkthiDjOFydYbutF8vP0npahy42YZaGoo");
+
+        var spotify = new SpotifyPrivateAPI();
+
+        
+        Console.WriteLine(await spotify.GetTrackInfo("59O0Xlgmh1no1j1PcdDfif"));
+    }
+}
+
+//https://api-partner.spotify.com/pathfinder/v1/query?operationName=getAlbum&variables=%7B%22uri%22%3A%22spotify%3Aalbum%3A4gTpoHQozYg3jI7KTbFsfx%22%2C%22locale%22%3A%22intl-pt%22%2C%22offset%22%3A0%2C%22limit%22%3A50%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2246ae954ef2d2fe7732b4b2b4022157b2e18b7ea84f70591ceb164e4de1b5d5d3%22%7D%7D
---
+++ SpotifyPrivateAPI.cs
@@ -0,0 +1,81 @@
+﻿
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+class SpotifyPrivateAPI
+{
+    private string baseURL = "https://api-partner.spotify.com/pathfinder/v1/query?";
+    private string? operationName = null;
+    private string? variables = null;
+    private string? extensions = null;
+    private string token;
+    private HttpClient client = new HttpClient();
+
+    private string GetQuery(string operationName, string variables, string extensions)
+    {
+        return "operationName=" + operationName + "&variables=" + variables + "&extensions=" + extensions;
+    }
+
+    private async Task<SpotifyToken> FetchAuthorization()
+    {
+        HttpClient tmpClient = new HttpClient();
+
+        SpotifyToken? result = null;
+
+        try {
+            result = await tmpClient.GetFromJsonAsync<SpotifyToken>("https://open.spotify.com/get_access_token?reason=transport&productType=web_player");
+        } catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+
+        if (result == null)
+        {
+            throw new Exception("Failed to fetch authorization token");
+        }
+
+        return result;
+
+    }
+
+    public SpotifyPrivateAPI(string? token = null)
+    {
+        if (token != null)
+        {
+            this.token = token;
+            return;
+        }
+
+        var auth = FetchAuthorization().Result;
+
+        this.token = auth.accessToken;
+    }
+
+
+    public async Task<string> GetTrackInfo(string trackID)
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        operationName = "getTrack";
+        variables = "%7B%22uri%22%3A%22spotify%3Atrack%3A" + trackID + "%22%2C%22locale%22%3A%22intl-pt%22%7D";
+        extensions = @"{
+            ""persistedQuery"": {
+                ""version"": 1,
+                ""sha256Hash"": ""e101aead6d78faa11d75bec5e36385a07b2f1c4a0420932d374d89ee17c70dd6""
+            }
+        }";
+
+        var data = await client.GetAsync(baseURL + GetQuery(operationName, variables, extensions));
+        return data.Content.ReadAsStringAsync().Result;
+
+    }
+
+    class SpotifyToken
+    {
+        public string clientId { get; set; }
+        public string accessToken { get; set; }
+        public long accessTokenExpirationTimestampMs { get; set; }
+        public bool isAnonymous { get; set; }
+    }
+}
